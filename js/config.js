ed95bda5dcb1d64f9cee2b08db6841ba4d8ebb07
Rakesh <rakeshl84@outlook.com>
2019-07-08 07:22:43 +1000

    message

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-west-2_A1sqt4PYE", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "1be1k4cifdvca17kifjkha0osa", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-west-2" // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
 };
